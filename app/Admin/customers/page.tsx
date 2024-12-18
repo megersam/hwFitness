54754d6c325da7c4977426797a2e0adbf58ee280
megersam <megersabmuleta01@gmail.com>
2024-12-18 16:15:50 +0300

    customers page for adding customer and search bar create.

--- app/Admin/customers/page.tsx
+++ app/Admin/customers/page.tsx
@@ -1,8 +1,29 @@
+import { Button } from '@/components/ui/button'
+import { Input } from '@/components/ui/input'
+import { Plus } from 'lucide-react'
 import React from 'react'
 
 const CustomersPage = () => {
   return (
-    <div>CustomersPage</div>
+    <div>
+      <div className='py-8'>
+        <Input
+          type="search"
+          placeholder="Search..."
+          className="md:w-[100px] lg:w-[900px]"
+        />
+      </div>
+
+      {/* Button for adding customers */}
+      <div className="absolute  right-10">
+        <Button>
+          <Plus size="lg" />
+          Add Customer
+        </Button>
+      </div>
+
+      {/* Table for users data display. */}
+    </div>
   )
 }
 
