d3e01d86cfad3844354154b3831818b3f32a192d
megersam <megersabmuleta01@gmail.com>
2024-12-19 19:33:31 +0300

    `Added search input and add employee button to EmployeePage component`

--- app/Admin/employee/page.tsx
+++ app/Admin/employee/page.tsx
@@ -1,8 +1,31 @@
+import { Button } from '@/components/ui/button'
+import { Input } from '@/components/ui/input'
+import { Plus } from 'lucide-react'
 import React from 'react'
 
 const EmployeePage = () => {
   return (
-    <div>EmployeePage</div>
+    <div className="px-4 sm:px-6 lg:px-8">
+        {/* Search Input */}
+        <div className="py-6 flex flex-col sm:flex-row sm:items-center sm:justify-between">
+        <Input
+          type="search"
+          placeholder="Search Employee..."
+          className="w-full sm:w-[80px] md:w-[140px] lg:w-[600px]"
+        />
+
+
+           {/* Button for adding customers */}
+           <div className="mt-4 sm:mt-0 sm:ml-4">
+          <Button 
+          
+          className="flex items-center space-x-2">
+            <Plus size={16} />
+            <span>Add Employee</span>
+          </Button>
+        </div>
+        </div>
+      </div>
   )
 }
 
