92ac5099586ccdc7d33389fa337b9de687c4231e
megersam <megersabmuleta01@gmail.com>
2024-12-23 15:59:27 +0300

    `Added state management and dialog for adding employees`

--- app/Admin/employee/page.tsx
+++ app/Admin/employee/page.tsx
@@ -1,11 +1,20 @@
+'use client'
 import { Button } from '@/components/ui/button'
 import { Input } from '@/components/ui/input'
 import { Plus } from 'lucide-react'
-import React from 'react'
+import React, { useState } from 'react'
 import EmployeeCard from './_components/employeeCard'
 import { employees } from './_components/employee';
+import { AddEmployeeDialog } from './_components/addEmployee'
 
 const EmployeePage = () => {
+    const [dialogVisible, setDialogVisible] = useState(false);
+  
+  
+    const handleAddEmployeeClick = () => setDialogVisible(true);
+    const closeDialog = () => setDialogVisible(false);
+
+
   return (
     <div className="px-4 sm:px-6 lg:px-8">
       {/* Search Input */}
@@ -20,12 +29,13 @@ const EmployeePage = () => {
         {/* Button for adding customers */}
         <div className="mt-4 sm:mt-0 sm:ml-4">
           <Button
-
+          onClick={handleAddEmployeeClick}
             className="flex items-center space-x-2">
             <Plus size={16} />
             <span>Add Employee</span>
           </Button>
         </div>
+        <AddEmployeeDialog visible={dialogVisible} onClose={closeDialog}/>
       </div>
       <div className='relative overflow-x-auto py-6'>
         {/* <EmployeeCard/> */}
