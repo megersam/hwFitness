1ea93ec4a263811f23313f80ee470b16d1498c09
megersam <megersabmuleta01@gmail.com>
2024-12-24 10:08:36 +0300

    `Added plans data to plans.js component`

---
+++ app/Admin/Plan/_components/plans.js
@@ -0,0 +1,48 @@
+export const plans  = [
+    {
+        "id": 1,
+        "name": "daily",
+        'duration': '1',
+        "date": 'oct-23-2024',
+        "discount": 'False',
+        "percent": 'N/A',
+        "status": 'Active',
+    },
+    {
+        "id": 2,
+        "name": "Monthly",
+        'duration': '1',
+        "date": 'oct-23-2024',
+        "discount": 'True',
+        "percent": '2%',
+        "status": 'Active',
+    },
+    {
+        "id": 3,
+        "name": "Monthly",
+        "duration": '3', 
+        "date": 'oct-23-2024',
+        "discount": 'True',
+        "percent": '5%',
+        "status": 'Active',
+    },
+    {
+        "id": 4,
+        "name": "Monthly",
+        "duration": 6, 
+        "date": 'oct-23-2024',
+        "discount": 'True',
+        "percent": '10%',
+        "status": 'Active',
+    },
+    {
+        "id": 4,
+        "name": "Monthly",
+        "duration": 6, 
+        "date": 'oct-23-2024',
+        "discount": 'True',
+        "percent": '10%',
+        "status": 'Active',
+    },
+   
+]
