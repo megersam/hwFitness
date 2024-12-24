a8d177a35387a0b4738b062aafedc8d59d5d8291
megersam <megersabmuleta01@gmail.com>
2024-12-24 10:54:51 +0300

    `feat: updated Admin/Plan/page.tsx with search input and layout changes`

--- app/Admin/Plan/page.tsx
+++ app/Admin/Plan/page.tsx
@@ -3,25 +3,39 @@ import SeachBar from './_components/serchBar'
 import { Button } from '@/components/ui/button'
 import { Plus } from 'lucide-react'
 import PlansTable from './_components/plansTable'
+import { Input } from '@/components/ui/input'
 
 const PlansPage = () => {
   return (
-    <div>
     <div className="px-4 sm:px-6 lg:px-8">
+    {/* Search Input */}
+    <div className="py-6 flex flex-col sm:flex-row sm:items-center sm:justify-between">
+      {/* <Input
+        type="search"
+        placeholder="Search..."
+        className="w-full sm:w-[80px] md:w-[140px] lg:w-[600px]"
+      /> */}
       
-  
-        {/* Button for adding Plans */}
-        <div className="ml-4">
-          <Button className="flex items-center space-x-2">
-            <Plus size={16} />
-            <span>Add Plans</span>
-          </Button>
-        </div>
-        <PlansTable/>
+      {/* Button for adding customers */}
+      <div className="mt-4 sm:mt-0 sm:ml-4">
+        <Button 
+         
+        className="flex items-center space-x-2">
+          <Plus size={16} />
+          <span>Add Plan</span>
+        </Button>
       </div>
-   
+      {/* <AddCustomerDialog visible={dialogVisible} onClose={closeDialog}/> */}
+    </div>
+
+    {/* Table for users data display */}
+    <div className="relative overflow-x-auto py-6">
+      
+        <PlansTable/>
+      
+    </div>
   </div>
-  
+
   )
 }
 
