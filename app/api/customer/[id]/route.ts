d0212ab224f523c6385fd04b87d33aeaeefe97e8
megersam <megersabmuleta01@gmail.com>
2025-01-02 15:13:02 +0300

    update population field from selectedPlan to planId in customer route

--- app/api/customer/[id]/route.ts
+++ app/api/customer/[id]/route.ts
@@ -32,7 +32,7 @@ export async function GET(
       customerId: id,
       startDate: { $lte: currentDate },
       endDate: { $gte: currentDate },
-    }).populate('selectedPlan', 'total'); 
+    }).populate('planId', 'total'); 
 
     console.log(activeSubscription);
     // Prepare the subscription details
